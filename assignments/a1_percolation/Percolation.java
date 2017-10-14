ff3ee1486f707b8f599a93821ccc4e5ad8385ab5
Anton Rouba <anton.rouba@gmail.com>
2017-10-14 06:37:44 -0700

    Temporary solution with 2-dim array

--- assignments/a1_percolation/Percolation.java
+++ assignments/a1_percolation/Percolation.java
@@ -3,23 +3,37 @@ package a1_percolation;
 public class Percolation {
     private int[][] id;
     private int openSites = 0;
-    private int side;
+    private int sideLength; // length of square side == N
+    private int[] topRow;
+    private int[] bottomRow;
 
     // create n-by-n grid, with all sites blocked
     public Percolation(int n) {
-        side = n;
-        id = new int[n][n];
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
-                id[i][j] = 0;
+        if (n > 0) {
+            sideLength = n;
+            id = new int[n][n];
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    id[i][j] = 0;
+        } else
+            throw new IllegalArgumentException();
     }
 
     // open site (row, col) if it is not open already
     public void open(int row, int col) {
-        if (id[row][col] == 0) {
-            id[row * col] = row * col;
-            openSites++;
+        int[] left = new int[]{row, col-1};
+        int[] right = new int[]{row, col+1};
+        int[] up = new int[]{row-1, col};
+        int[] down = new int[]{row+1, col-1};
+
+        if((row > 0) && (col > 0)) {
+            if (id[row][col] == 0) {
+                id[row][col] = row * sideLength + (col + 1);
+                openSites++;
+            }
         }
+        else
+            throw new IllegalArgumentException();
     }
 
     // is site (row, col) open?
@@ -30,8 +44,8 @@ public class Percolation {
 
     // is site (row, col) full?
     public boolean isFull(int row, int col) {
-        for (int i = 0; i < side - 1; i++)
-            if (id[row*col] == i)
+        for (int i = 0; i < sideLength - 1; i++)
+            if (id[row][col] == i)
                 return true;
 
         return false;
@@ -46,9 +60,40 @@ public class Percolation {
 
     // does the system percolate?
     public boolean percolates() {
-
+        for (int i = 0; i < sideLength; i++)
+            if (isFull(sideLength, i))
+                return true;
 
         return false;
+    }
+
+    public int root(int row, int col) {
+        int[] coords;
+        int r = row;
+        int c = col;
+        while (getIndex(r, c) != id[r][c]) {
+            coords = getCoords(id[r][c]);
+            r = coords[0];
+            c = coords[1];
+        }
+
+        return getIndex(r, c);
+    }
+
+    private int getIndex(int row, int col) {
+        return row * sideLength + (col + 1);
+    }
+
+    private int[] getCoords(int index) {
+        int[] coords = new int[2];
+        coords[0] = index / sideLength;
+        coords[1] = index - 1 - sideLength * coords[0];
+
+        return coords;
+    }
+
+    public void setIndex(int row, int col, int index) {
+        id[row][col] = index;
     }
 
     // test client (optional)
